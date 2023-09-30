d67d965ea6cca32a81f7c59f1b9bc05eddfaa6a6
Tim Halliday <thalliday793@gmail.com>
2023-09-30 10:35:50 +0100

    select random solution word on render

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,25 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Header from './components/header/Header'
 
-import { generate, count } from "random-words";
-
 function App() {
-  const [solution, setSolution] = useState(generate({ minLength: 5, maxLength: 5 }));
+  const [solution, setSolution] = useState(null);
+
+  useEffect(() => {
+    console.log("run use effect")
+    fetch("http://localhost:3001/solutions")
+    .then(response => response.json())
+    .then(data => {
+      const randomWord = data[Math.floor(Math.random() * data.length)]// Select random word
+      setSolution(randomWord.word)
+      })
+  }, [setSolution])
 
   return (
     <>
+      {solution}
       <Header/>
     </>
   )
